0902a1fd80655986c7a3d81cba2693360ca7a6c1
MHuzaifaArshad <[muhammadhhuzaifa130@gmail.com]>
2025-08-12 20:45:55 +0500

    Modified vite.config for Lovable deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-//import { componentTagger } from "lovable-tagger";
+import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+
 export default defineConfig(({ mode }) => ({
   server: {
-    host: mode === 'development' ? "localhost" : "0.0.0.0",
-    port: mode === 'development' ? 3000 : 8080,
+    host: mode === "::",
+    port: 8080,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    
+     mode === 'development' &&
+     componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // Production optimizations
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser',
-  }
 }));
